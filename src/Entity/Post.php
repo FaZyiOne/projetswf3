a1e9917a5aab6ea6e3fd02b1a88b9c2f1e9e137e
IvanBF9 <ivanbraga@outlook.fr>
2020-01-09 15:06:40 +0100

    base

---
+++ src/Entity/Post.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $commentaire;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    
+    private $user;
+
+    private $reservation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(string $commentaire): self
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getUser(): ?string
+    {
+        return $this->user;
+    }
+
+    public function setUser(string $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getReservation(): ?string
+    {
+        return $this->reservation;
+    }
+
+    public function setReservation(string $reservation): self
+    {
+        $this->reservation = $reservation;
+
+        return $this;
+    }
+}
