5fbb80bba9fc92bae337c73383c25e156dab2f4f
fzabid05 <abidzora88@gmail.com>
2020-01-10 17:31:55 +0100

    reglage accueil

--- public/js/program.js
+++ public/js/program.js
@@ -1,13 +1,44 @@
+// menu ---------------------------------
+
+  
+
+
+// nav
+
+
 $(document).ready(function(){
     $(window).scroll(function(){
       var scroll = $(window).scrollTop();
       if (scroll < 100) {
-        $("nav.navbar").css("background" , "rgba(255, 255, 255, 0.1)");
+        $("nav").css("background" , "rgba(255, 255, 255, 0.5)");
       }
   
       else{
-        $("nav.navbar").css("background" , "white");  	
+        $("nav").css("background" , "white");  	
       }
       
     })
+
+
   });
+  
+
+  (function(){
+    const burger = document.querySelector(".burger");
+    burger.onclick = function()
+    {
+      const menu = document.querySelector(".menu");
+      
+      if(menu.dataset.etat == "open") // si le menu est ouvert => fermer
+      {
+        menu.classList.add("open");
+        menu.dataset.etat = "close";
+      }
+      else
+      {
+        menu.classList.remove("open") ;
+        menu.dataset.etat = "open";
+      }
+    }
+  })();
+  
