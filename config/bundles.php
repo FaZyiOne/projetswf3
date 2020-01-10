f9091c4460095aee824eeca7bbac6c47bd3c00ff
FaZyiOne <alix.queinnec@gmail.com>
2020-01-10 14:06:59 +0100

    bundlesIgnior

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,8 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 
+
+
+
     
 ];
