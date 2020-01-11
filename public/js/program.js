3d19790891b3e2682ff7f1fe175d0ff1c9c04a82
fzabid05 <abidzora88@gmail.com>
2020-01-11 16:34:40 +0100

    accueil

--- public/js/program.js
+++ public/js/program.js
@@ -1,30 +1,32 @@
 // nav
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//     var scroll = $(window).scrollTop();
+//     if (scroll < 100) {
+//       $("nav").css("background", "blue");
+//     } else {
+//       $("nav").css("background", "pink");
+//     }
+//   })
 
+// });
 
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//       var scroll = $(window).scrollTop();
-//       if (scroll < 100) {
-//         $("nav").css("background" , "rgba(255, 255, 255, 0.5)");
-//       }
-  
-//       else{
-//         $("nav").css("background" , "white");  	
-//       }
-      
-//     })
+// -- hover
 
+$(function () {
 
-//   });
-  
+  $('ul.hover_block li').hover(function () {
+    $(this).find('img').animate({
+      opacity: "0.7"
+    }, {
+      duration: 300
+    });
+  }, function () {
+    $(this).find('img').animate({
+      opacity: "1"
+    }, {
+      duration: 300
+    });
+  });
 
-  
-  $(function() {
- 
-    $('ul.hover_block li').hover(function() {
-        $(this).find('img').animate({opacity: "0.2"}, {duration:300});
-        }, function(){
-            $(this).find('img').animate({opacity: "1"}, {duration:300});
-        });
-    
-});  
+});
