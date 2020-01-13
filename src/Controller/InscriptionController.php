a2b3ab506b62c137a46e788183739fda25707306
malikmlk <57346526+malikmlk@users.noreply.github.com>
2020-01-13 15:32:00 +0100

    ajout page inscription

---
+++ src/Controller/InscriptionController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Inscription;
+use App\Form\InscriptionType;
+use App\Repository\InscriptionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/inscription")
+ */
+class InscriptionController extends AbstractController
+{
+    /**
+     * @Route("/", name="inscription_index", methods={"GET"})
+     */
+    public function index(InscriptionRepository $inscriptionRepository): Response
+    {
+        return $this->render('inscription/index.html.twig', [
+            'inscriptions' => $inscriptionRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="inscription_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $inscription = new Inscription();
+        $form = $this->createForm(InscriptionType::class, $inscription);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($inscription);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('inscription_index');
+        }
+
+        return $this->render('inscription/new.html.twig', [
+            'inscription' => $inscription,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="inscription_show", methods={"GET"})
+     */
+    public function show(Inscription $inscription): Response
+    {
+        return $this->render('inscription/show.html.twig', [
+            'inscription' => $inscription,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="inscription_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Inscription $inscription): Response
+    {
+        $form = $this->createForm(InscriptionType::class, $inscription);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('inscription_index');
+        }
+
+        return $this->render('inscription/edit.html.twig', [
+            'inscription' => $inscription,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="inscription_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Inscription $inscription): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$inscription->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($inscription);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('inscription_index');
+    }
+}
