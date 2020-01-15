10e7e654116f232ebcc12eba3c754257525df062
malikmlk <57346526+malikmlk@users.noreply.github.com>
2020-01-15 10:34:45 +0100

    ajout page contact

---
+++ src/Form/ContactType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('numero_de_telephone')
+            ->add('societe')
+            ->add('email')
+            ->add('sujet')
+            ->add('message')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
