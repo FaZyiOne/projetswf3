4f0dcf33ed69114ff7fe6a7ecd17957775996721
Git-raymond <raymond.saray@yahoo.com>
2020-01-15 22:46:17 +0100

    master du 15/01 + Malik

---
+++ src/Entity/Contact.php
@@ -0,0 +1,110 @@
+<?php
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $numero_de_telephone;
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $societe;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sujet;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $message;
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+    public function getNumeroDeTelephone(): ?string
+    {
+        return $this->numero_de_telephone;
+    }
+    public function setNumeroDeTelephone(string $numero_de_telephone): self
+    {
+        $this->numero_de_telephone = $numero_de_telephone;
+        return $this;
+    }
+    public function getSociete(): ?string
+    {
+        return $this->societe;
+    }
+    public function setSociete(?string $societe): self
+    {
+        $this->societe = $societe;
+        return $this;
+    }
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+    public function getSujet(): ?string
+    {
+        return $this->sujet;
+    }
+    public function setSujet(string $sujet): self
+    {
+        $this->sujet = $sujet;
+        return $this;
+    }
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+        return $this;
+    }
+}
