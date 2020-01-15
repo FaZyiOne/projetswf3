b8f4532242901104b9afccbfb8e7825011441910
Git-raymond <raymond.saray@yahoo.com>
2020-01-15 07:40:31 +0100

    paiement par CB stripe

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -5,14 +5,40 @@ namespace App\Controller;
 use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle;
+use Symfony\Component\HttpFoundation\Request;
 
-class SallesController extends AbstractController
+class PaymentController extends AbstractController
 {
     /**
      * @Route("/payment", name="payment")
      */
-    public function index(ReservationRepository $reservationRepository)
+    public function indexAction()
     {
-        return $this->render('payment/index.html.twig', []);
+        // Set your secret key: remember to change this to your live secret key in production
+        // See your keys here: https://dashboard.stripe.com/account/apikeys
+        \Stripe\Stripe::setApiKey('sk_test_oazArn1Rva1TYTJx7zbpCcst00K1PE0LHL');
+        
+        // Token is created using Stripe Checkout or Elements!
+        // Get the payment token ID submitted by the form:
+        
+        if (isset($_POST['stripeToken'])) {
+            $token = $_POST['stripeToken'];
+            $charge = \Stripe\Charge::create([
+                'amount' => 999,
+                'currency' => 'eur',
+                'description' => 'Example charge',
+                'source' => $token,
+              ]);
+        } else {
+            $errors['token'] = 'The order cannot be processed. Please make sure you have JavaScript enabled and try again.';
+        }
+
+           
+
+        return $this->render('payment/index.html.twig');
+        
     }
 }
+
+
