63a6a3cd6b29121cefdfee4c6a6c157632fcf592
Git-raymond <raymond.saray@yahoo.com>
2020-01-17 08:56:44 +0100

    contact

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -1,110 +1,162 @@
 <?php
 namespace App\Entity;
-use Doctrine\ORM\Mapping as ORM;
-/**
- * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
- */
+
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Contact
 {
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $nom;
-    /**
-     * @ORM\Column(type="string", length=255)
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min=2, max=100)
      */
     private $prenom;
+
     /**
-     * @ORM\Column(type="string", length=255)
+     * @var string|null
+     * @Assert\NotBlank
+     * @Assert\Length(min=2, max=100)
      */
-    private $numero_de_telephone;
+    private $nom;
+
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     * pattern"/[0-9]{10}/"
+     * )
      */
-    private $societe;
+    private $telephone;
+
     /**
-     * @ORM\Column(type="string", length=255)
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Email()
      */
     private $email;
+
     /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $sujet;
-    /**
-     * @ORM\Column(type="string", length=255)
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min=10)
      */
     private $message;
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-    public function getNom(): ?string
-    {
-        return $this->nom;
-    }
-    public function setNom(string $nom): self
-    {
-        $this->nom = $nom;
-        return $this;
-    }
-    public function getPrenom(): ?string
+
+    // /**
+    //  * @var Property|null
+    //  */
+    // private $property;
+
+
+    /**
+     * @return null|string  
+     */
+     public function getPrenom(): ?string
     {
         return $this->prenom;
     }
-    public function setPrenom(string $prenom): self
+
+    /**
+     * @param null|string $prenom
+     * @return Contact
+     */
+    public function setPrenom(?string $prenom): Contact
     {
         $this->prenom = $prenom;
         return $this;
     }
-    public function getNumeroDeTelephone(): ?string
+
+
+    /**
+     * @return null|string  
+     */
+     public function getNom(): ?string
     {
-        return $this->numero_de_telephone;
+        return $this->nom;
     }
-    public function setNumeroDeTelephone(string $numero_de_telephone): self
+
+    /**
+     * @param null|string $nom
+     * @return Contact
+     */
+    public function setNom(?string $nom): Contact
     {
-        $this->numero_de_telephone = $numero_de_telephone;
+        $this->nom = $nom;
         return $this;
     }
-    public function getSociete(): ?string
+
+    /**
+     * @return null|string  
+     */
+     public function getTelehone(): ?string
     {
-        return $this->societe;
+        return $this->telephone;
     }
-    public function setSociete(?string $societe): self
+
+   /**
+     * @param null|string $telephone
+     * @return Contact
+     */
+    public function setTelephone(?string $telephone): Contact
     {
-        $this->societe = $societe;
+        $this->telephone = $telephone;
         return $this;
     }
-    public function getEmail(): ?string
+
+
+    /**
+     * @return null|string  
+     */
+     public function getEmail(): ?string
     {
         return $this->email;
     }
-    public function setEmail(string $email): self
+
+    /**
+     * @param null|string $email
+     * @return Contact
+     */
+    public function setEmail(?string $email): Contact
     {
         $this->email = $email;
         return $this;
     }
-    public function getSujet(): ?string
-    {
-        return $this->sujet;
-    }
-    public function setSujet(string $sujet): self
-    {
-        $this->sujet = $sujet;
-        return $this;
-    }
-    public function getMessage(): ?string
+
+
+    /**
+     * @return null|string  
+     */
+     public function getMessage(): ?string
     {
         return $this->message;
     }
-    public function setMessage(string $message): self
+
+    /**
+     * @param null|string $message
+     * @return Contact
+     */
+    public function setMessage(?string $message): Contact
     {
         $this->message = $message;
         return $this;
     }
-}
+
+
+    // /**
+    //  * @return Property|null
+    //  */
+    //  public function getProperty(): ?Property
+    // {
+    //     return $this->property;
+    // }
+
+    // /**
+    //  * @param Property|null $property
+    //  * @return Contact
+    //  */
+    // public function setProperty(?Property $property): Contact
+    // {
+    //     $this->property = $property;
+    //     return $this;
+    // }
+}   
